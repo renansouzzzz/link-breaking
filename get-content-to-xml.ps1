03243dbc3639060506e0a1ffa944fc17b218fac6
Renan de Oliveira <85078612+renansouzzzz@users.noreply.github.com>
2024-10-25 13:19:09 -0300

    get contents (links) and append to xlsx contents

---
+++ get-content-to-xml.ps1
@@ -0,0 +1,27 @@
+﻿param(
+    [string]$pageName
+)
+
+$siteUrl = ""
+$outputFile = ""
+
+$appId = ""
+$appSecret = ""
+
+Connect-PnPOnline -Url $siteUrl -UseWebLogin
+
+$page = Get-PnPFile -Url "" -AsString
+
+$xmlDoc = New-Object System.Xml.XmlDocument
+$root = $xmlDoc.CreateElement("SharepointPage")
+$xmlDoc.AppendChild($root)
+
+$pageContent = $xmlDoc.CreateElement("PageContent")
+$pageContent.InnerText = $page
+$root.AppendChild($pageContent)
+
+$xmlDoc.Save($outputFile)
+
+Write-Host "A página foi exportada com sucesso para $outputFile"
+
+& .\remove-caract-xml2.ps1 -pageName $pageName
---
+++ get-links-by-content-xlsx-upd.ps1
@@ -0,0 +1,54 @@
+﻿param(
+    [string]$pageName
+)
+
+$xmlFiles = @(
+    ""
+)
+$outputExcelFile = ""
+
+if (-Not (Get-Module -ListAvailable -Name ImportExcel)) {
+    Write-Host "O módulo ImportExcel não está instalado. Execute 'Install-Module -Name ImportExcel' para instalá-lo."
+    exit
+}
+
+foreach ($xmlFile in $xmlFiles) {
+    try {
+        $xmlContent = Get-Content $xmlFile -Raw
+        Write-Host "Arquivo XML '$xmlFile' carregado com sucesso."
+    } catch {
+        Write-Host "Erro ao carregar o arquivo XML: $_"
+        continue
+    }
+
+    $links = @()
+    $pattern = '<a\s+href="([^"]+)"[^>]*>(.*?)<\/a>'
+    if ($xmlContent -match $pattern) {
+        $matches = [regex]::Matches($xmlContent, $pattern)
+        foreach ($match in $matches) {
+            $link = $match.Groups[1].Value
+            $name = $match.Groups[2].Value -replace '<\/?u>', ''
+            $links += [PSCustomObject]@{
+                "Título da Página" = $pageName -replace '.aspx', ''
+                "Título do Link" = $name
+                "URL do Link"    = $link
+            }
+        }
+    }
+
+    $pageName = [System.IO.Path]::GetFileNameWithoutExtension($xmlFile) -replace '.aspx', ''
+
+    if ($links.Count -eq 0) {
+        Write-Host "Nenhum link encontrado no conteúdo do arquivo '$xmlFile'."
+    } else {
+        if (Test-Path $outputExcelFile) {
+            $links | Export-Excel -Path $outputExcelFile -WorkSheetname $pageName -AutoSize -Append
+        } else {
+            $links | Export-Excel -Path $outputExcelFile -WorkSheetname $pageName -AutoSize
+        }
+        Write-Host "Links extraídos da página '$pageName' e salvos na planilha: $outputExcelFile"
+    }
+
+    Remove-Item -Path $xmlFile -Force
+
+}
